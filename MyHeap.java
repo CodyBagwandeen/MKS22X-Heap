6f3aeaf8a90d35a13cbfafe64feed0fb5aa746f3
CodyBagwandeen <cbagwandeen@stuy.edu>
2019-04-29 11:47:43 -0400

    did something

--- MyHeap.java
+++ MyHeap.java
@@ -2,6 +2,17 @@ public class MyHeap{
 
   private static void pushDown(int[]data, int size, int index){
 
+    if(2 * index +1 > data.length){
+      return;
+    }
+
+    int left = 2 * index +1;
+    int right = 2 * index +2;
+
+    if(data[index] > data[left] && data[index] > data[right]){
+      return;
+    }
+
   }
 
   private static void pushUp(int[]data, int size, int index){
@@ -15,4 +26,5 @@ public class MyHeap{
   public static void heapsort(int[]data){
 
   }
+
 }
