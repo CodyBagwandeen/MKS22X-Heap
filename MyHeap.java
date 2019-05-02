6cb8d8cccf7cf5c7f7c09c124ad92741e93a8516
CodyBagwandeen <cbagwandeen@stuy.edu>
2019-05-01 23:05:38 -0400

    i got push down to work and since it works heapify and heapsort work

--- MyHeap.java
+++ MyHeap.java
@@ -1,24 +1,33 @@
+import java.util.*;
 public class MyHeap{
 
   private static void pushDown(int[]data, int size, int index){
 
-    if(2 * index +1 > data.length){ // if it has no children
-      return;
-    }
+    int parent = index;
+    index = 2 * index + 1;
 
-    int left = 2 * index +1;
-    int right = 2 * index +2;
+    while( index < size){ // if a child exists
+      int left = data[index];
+      int max = left;
 
-    if(data[index] > data[left] && data[index] > data[right]){ // if is bigger than its children
-      return;
-    }
+      if( size > index +1){ // see if the other chuld exists
+        int right = data[index+1];
 
-    if( data[left] > data[right]) { // left child is the biggest of the 3
-      swap(data, left, index);
-      pushDown(data, data.length, left);
-    } else {
-      swap(data,right,index);
-      pushDown(data, data.length,right);
+        if(left < right){
+          max = right; // the biggest is now the right and the index must change to reflect that
+          index++;
+        }
+      }
+
+      if( data[parent] < max){
+        swap(data,parent,index); // swaps the parent and the superior child
+        parent = index; // now the child is the one being looked at
+      } else {
+        return; // if the parent is the biggest there is no need to keep going
+      }
+
+      index = 2 * index + 1;
+
     }
 
   }
@@ -52,4 +61,25 @@ public class MyHeap{
     data[j] = temp;
   }
 
+  public static void main(String[] args){
+    int[] data =  {5,1,2,6,123};
+
+    System.out.println(Arrays.toString(data));
+
+    heapify(data);
+
+    System.out.println(Arrays.toString(data));
+    System.out.println("Should be [123, 6, 2, 5, 1]");
+    int[] data1 =  {5,1,2,6,123,12,15,13,18,999};
+    int[] data2 =  {5,1,2,6,123,12,15,13,18,999};
+
+    Arrays.sort(data1);
+
+    heapsort(data2);
+
+    System.out.println(Arrays.toString(data1));
+    System.out.println(Arrays.toString(data2));
+
+
+  }
 }
